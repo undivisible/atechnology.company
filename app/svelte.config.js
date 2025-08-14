2c003cab3bf99e0825bcee0d4945752ec5d18ccc
Max Lee Abdurrahman As-Salafi Carter 祁明思 <undivisible@vk.com>
2025-08-14 14:47:56 +1000

    hotfix

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,12 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+const dev = process.env.NODE_ENV === 'development';
+export default {
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: ''
+    }
+  }
 };
-
-export default config;
